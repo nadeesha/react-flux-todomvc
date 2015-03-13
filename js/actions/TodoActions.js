a3a9a309ba23c629178ae9c0ba844eb740d763ee
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2015-03-13 07:17:16 +0530

    Initial commit

---
+++ js/actions/TodoActions.js
@@ -0,0 +1,19 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var TodoConstants = require('../constants/TodoConstants');
+
+var TodoActions = {
+	create: function (text) {
+		AppDispatcher.handleViewAction({
+			actionType: TodoConstants.TODO_CREATE,
+			text: text
+		});
+	},
+	destroy: function (id) {
+		AppDispatcher.handleViewAction({
+			actionType: TodoConstants.TODO_DESTROY,
+			id: id
+		});
+	} // change
+};
+
+module.exports = TodoActions;
---
+++ js/constants/TodoConstants.js
@@ -0,0 +1,6 @@
+var TodoConstants = {
+	TODO_CREATE: 'create',
+	TODO_DESTROY: 'destroy'
+};
+
+module.exports = TodoConstants;
